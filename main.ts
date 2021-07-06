818b78c6544c45f037d8f14e2a5412c06a8264a0
joaopjk <joaopjk48@gmail.com>
2021-07-06 00:06:19 -0300

    Configurando Restify

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log("main script");
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name:'meat-api',
+    version:'1.0.0'
+});
+
+server.get('/hello',(req,res,next)=>{
+   res.json({message:'hello'});
+   return next();
+});
+
+server.listen(3000,()=>{
+    console.log("Api is running on http://localhost:3000 ");
+});
