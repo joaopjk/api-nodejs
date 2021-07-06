42c64c9e5ec1cdae3170bc7a8266b90cea01de21
joaopjk <joaopjk48@gmail.com>
2021-07-06 20:28:14 -0300

    User router

--- users/user.router.ts
+++ users/user.router.ts
@@ -4,9 +4,22 @@ import {User} from "./users.model";
 
 class UsersRouters extends Router {
     applyRoutes(application: restify.Server) {
+
         application.get('/users', (req, res, next) => {
             User.findAll().then( users=>{
                 res.json(users);
+                return next();
+            })
+        });
+
+        application.get('/users/:id',(req,res,next)=>{
+            User.findById(req.params.id).then(user=>{
+                if(user) {
+                    res.json(user);
+                    return next();
+                }
+
+                res.send(404);
                 return next();
             })
         })
--- users/users.model.ts
+++ users/users.model.ts
@@ -1,10 +1,22 @@
 const users = [
-    {name: 'João', email: "joao@gmail.com"},
-    {name: 'Francini', email: "francini@gmail.com"}
+    {id: "1", name: 'João', email: "joao@gmail.com"},
+    {id: "2", name: 'Francini', email: "francini@gmail.com"}
 ];
 
 export class User {
     static findAll(): Promise<any[]> {
         return Promise.resolve(users);
+    }
+
+    static findById(id: string): Promise<any> {
+        return new Promise(resolve => {
+            const filtered = users.filter(u => u.id === id);
+            let user = undefined;
+
+            if (filtered.length > 0)
+                user = filtered[0]
+
+            resolve(user);
+        })
     }
 }
