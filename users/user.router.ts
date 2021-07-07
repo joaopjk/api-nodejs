4aaef241d95f8c1fd6c25b21dd09da6827afbdd5
joaopjk <joaopjk48@gmail.com>
2021-07-06 22:31:28 -0300

    User Delete

--- users/user.router.ts
+++ users/user.router.ts
@@ -47,6 +47,30 @@ class UsersRouters extends Router {
                 return next();
             });
         });
+
+        application.patch("/users/:id", (req, res, next) => {
+            const options = {new: true};
+            User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
+                if (user) {
+                    res.json(user);
+                    return next();
+                }
+                res.send(404);
+                return next();
+            })
+        });
+
+        application.del('users/:id', (req, res, next) => {
+            User.remove({_id: req.params.id}).exec()
+                .then((result: any) => {
+                    if (result.result.n) {
+                        res.send(204);
+                    } else {
+                        res.send(404);
+                    }
+                    return next();
+                })
+        })
     }
 }
 
