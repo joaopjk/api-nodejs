0f9a1fe8a7e544f73e869b90eceda53a83667366
joaopjk <joaopjk48@gmail.com>
2021-07-06 21:22:39 -0300

    User get router

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,22 +1,17 @@
-const users = [
-    {id: "1", name: 'João', email: "joao@gmail.com"},
-    {id: "2", name: 'Francini', email: "francini@gmail.com"}
-];
+import * as mongoose from 'mongoose';
 
-export class User {
-    static findAll(): Promise<any[]> {
-        return Promise.resolve(users);
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        select: false
     }
+});
 
-    static findById(id: string): Promise<any> {
-        return new Promise(resolve => {
-            const filtered = users.filter(u => u.id === id);
-            let user = undefined;
-
-            if (filtered.length > 0)
-                user = filtered[0]
-
-            resolve(user);
-        })
-    }
-}
+export const User = mongoose.model('User', userSchema);
