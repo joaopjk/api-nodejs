71736fc3e94200d4a744a44058678d0296a4d775
joaopjk <joaopjk48@gmail.com>
2021-07-07 23:55:02 -0300

    Refatorações

--- common/router.ts
+++ common/router.ts
@@ -3,18 +3,29 @@ import {EventEmitter} from 'events';
 import {NotFoundError} from "restify-errors";
 
 export abstract class Router extends EventEmitter {
-    abstract applyRoutes(application:restify.Server)
+    abstract applyRoutes(application: restify.Server)
 
-    render(response: restify.Response,next:restify.Next){
-        return (document)=>{
-            if(document) {
-                this.emit('beforeRender',document);
+    render(response: restify.Response, next: restify.Next) {
+        return (document) => {
+            if (document) {
+                this.emit('beforeRender', document);
                 response.json(document);
-            }
-            else
+            } else
                 throw new NotFoundError('Documento não encontrado !');
-
             return next();
+        }
+    }
+
+    renderAll(response: restify.Response, next: restify.Next) {
+        return (documents: any[]) => {
+            if (documents) {
+                documents.forEach(document => {
+                    this.emit('beforeRender', document)
+                });
+                response.json(documents);
+            } else {
+                response.json([]);
+            }
         }
     }
 }
--- users/user.router.ts
+++ users/user.router.ts
@@ -1,11 +1,10 @@
 import * as restify from "restify";
-import {NotFoundError} from "restify-errors";
-import {Router} from "../common/router";
 import {User} from "./users.model";
+import {ModelRouter} from "../common/model-router";
 
-class UsersRouters extends Router {
+class UsersRouters extends ModelRouter<User> {
     constructor() {
-        super();
+        super(User);
         this.on('beforeRender', document => {
             document.password = undefined;
         })
@@ -13,53 +12,12 @@ class UsersRouters extends Router {
 
     applyRoutes(application: restify.Server) {
 
-        application.get('/users', (req, res, next) => {
-            User.find().then(this.render(res, next))
-                .catch(next);
-        });
-
-        application.get('/users/:id', (req, res, next) => {
-            User.findById(req.params.id).then(this.render(res, next))
-                .catch(next);
-        });
-
-        application.post('/users', (req, res, next) => {
-            let user = new User(req.body);
-            user.save().then(this.render(res, next))
-                .catch(next);
-        });
-
-        application.put("/users/:id", (req, res, next) => {
-            const options = {overwrite: true};
-            User.update({_id: req.params.id}, req.body, options)
-                .exec().then(result => {
-                if (result.n) {
-                    return User.findById(req.params.id).exec();
-                } else {
-                    throw new NotFoundError('Documento não encontrado !');
-                }
-            }).then(this.render(res, next))
-                .catch(next);
-        });
-
-        application.patch("/users/:id", (req, res, next) => {
-            const options = {new: true};
-            User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(res, next))
-                .catch(next);
-        });
-
-        application.del('users/:id', (req, res, next) => {
-            User.remove({_id: req.params.id}).exec()
-                .then((result: any) => {
-                    if (result.result.n) {
-                        res.send(204);
-                    } else {
-                        throw new NotFoundError('Documento não encontrado !');
-                    }
-                    return next();
-                })
-                .catch(next)
-        })
+        application.get('/users', this.findAll)
+        application.get('/users/:id', [this.validateId, this.findById]);
+        application.post('/users', this.save);
+        application.put("/users/:id", [this.validateId, this.replace]);
+        application.patch("/users/:id", [this.validateId, this.update]);
+        application.del('users/:id', [this.validateId, this.delete])
     }
 }
 
